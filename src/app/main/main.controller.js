8f70cbdea1f3e0fc6306d30961115a09c1b2a571
ermaxw <ermaxw@gmail.com>
2015-05-20 15:38:48 -0500

    Fixed dynamic loading of commits. : 192.168.1.134
    : f8:16:54:7d:94:75

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,64 +1,44 @@
 'use strict';
 
 angular.module('sv')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
-        'title': 'Angular UI Bootstrap',
-        'url': 'http://angular-ui.github.io/bootstrap/',
-        'description': 'Bootstrap components written in pure AngularJS by the AngularUI Team.',
-        'logo': 'ui-bootstrap.png'
-      },
-      {
-        'title': 'Sass (Node)',
-        'url': 'https://github.com/sass/node-sass',
-        'description': 'Node.js binding to libsass, the C version of the popular stylesheet preprocessor, Sass.',
-        'logo': 'node-sass.png'
-      }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+  .controller('MainCtrl', function ($scope, GithubAPI) {
+    $scope.user = {'username': '', 'repos':[], 'branches':[], 'currentRepo':'', 'currentBranch': 'master', 'currentCommits':[]}
+    $scope.usernameChange = function(){
+        GithubAPI.getRepos($scope.user.username).then(function(data){
+            $scope.user.repos = [];
+            angular.forEach(data, function(repo){
+                $scope.user.repos.push(repo.full_name.split("/").pop());
+            });
+        });
+    }
+    
+    $scope.repoChanged = function(){
+        $scope.user.branches = [];
+        GithubAPI.getBranches($scope.user.username, $scope.user.currentRepo).then(function(data){
+            angular.forEach(data, function(branch){
+                $scope.user.branches.push(branch.name);
+            });
+        });
+    }
+
+
+    $scope.branchChanged = function(){
+        GithubAPI.getCommits($scope.user.username, $scope.user.currentRepo, $scope.user.currentBranch).then(function(data){
+            $scope.user.currentCommits = [];
+            //"This is my test change for commit : 192.168.1.134\n\n: f8:16:54:7d:94:75",
+
+            angular.forEach(data, function(commit){
+                var commits = commit.commit.message.split(':');
+                var ip = '';
+                var mac = '';
+                if(commits[1]){
+                    ip = commits[1].replace(/(\r\n|\n|\r)/gm,"");
+                }
+                if(commits[2]){
+                    mac = commit.commit.message.split(':').slice(2).join(':');
+                }
+                $scope.user.currentCommits.push({'timestamp': commit.commit.committer.date , 'mac': mac, 'ip': ip, 'commit': commit.sha});
+            });
+        });
+    }
   });
